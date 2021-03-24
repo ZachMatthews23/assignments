17f223f504bf714075ff2e50f79318ad9e9bed19
Zachary Matthews <zacharyyog12@gmail.com>
2021-03-23 17:29:24 -0700

    Added delte button and edit button to moms shopping list

--- Level 1/Moms Shopping List/moms-shopping-list-start/index.js
+++ Level 1/Moms Shopping List/moms-shopping-list-start/index.js
@@ -2,20 +2,47 @@ const form = document.addItem
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
+
     const input = form.title.value
     form.title.value = ""
-    const li = document.createElement("li")
-    const div = document.createElement("div")
-    const edit = document.createElement("button")
-    const x = document.createElement("button")
-    div.textContent = input
-    edit.textContent = "edit"
-    x.textContent = "X"
     const ul = document.getElementById("list")
-    div.setAttribute("id", "list")
+    
+    const li = document.createElement("li")
+    li.setAttribute("id", "item")
     ul.append(li)
+
+    const div = document.createElement("div")
+    div.textContent = input
+    div.classList.add("newItem")
     li.append(div)
+    
+    const edit = document.createElement("button")
+    edit.textContent = "edit"
+    edit.style.marginRight = "4px"
     li.append(edit)
-    li.append(x)
+    
+    const deleteButton = document.createElement("button")
+    deleteButton.textContent = "X"
+    deleteButton.classList.add("delete")
+    li.append(deleteButton)
+    
+    deleteButton.addEventListener("click", function(){
+        div.remove()
+        edit.remove()
+        deleteButton.remove()
+        li.remove
+    })
+
+    edit.addEventListener("click", function(event){
+        event.preventDefault()
+        var input = document.createElement("input")
+        div.contentEditable = "true";
+        var newDiv = input.value    
+    })
 })
+
+
+
+
+
 
