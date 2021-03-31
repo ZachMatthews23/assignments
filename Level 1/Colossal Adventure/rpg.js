80d74fb4baafce407d7661c3f7e5a45b191c30ed
Zachary Matthews <zacharyyog12@gmail.com>
2021-03-30 20:38:31 -0700

    Added fight function and updated walking and random enemy function.

--- Level 1/Colossal Adventure/rpg.js
+++ Level 1/Colossal Adventure/rpg.js
@@ -12,7 +12,7 @@ const player = {
 // console.log(player)
 
 // Constructor function to create the enemies
-function Enemies(name, hp, attack, hpPotion){
+function Enemy(name, hp, attack, hpPotion){
     this.name = name,
     this.hp = hp,
     this.attack = attack
@@ -20,49 +20,17 @@ function Enemies(name, hp, attack, hpPotion){
 }
 
 // Enemies
-const enemiesArr = [];
-const goblin = new Enemies("Goblin", 35, 25, 10)
-const ghoul = new Enemies("Ghoul", 60, 35, 20)
-const ogre = new Enemies("Ogre", 75, 45, 30)
+// Array with the enemies pushed into it
+let enemiesArr = [];
+const goblin = new Enemy("Goblin", 35, 25, 10)
+const ghoul = new Enemy("Ghoul", 35, 35, 20)
+const ogre = new Enemy("Ogre", 35, 45, 30)
 enemiesArr.push(goblin, ghoul, ogre);
 
-// Array with the enemies pushed into it
-// console.log(enemiesArr)
 
-// Player walking with 1/3 chance of an enemy attacking.
-function walking(){
-    let attackOrWalk = Math.random();
-    if(attackOrWalk < 0.35){
-        enemyAmbush();
-    } else {
-        readlineSync.keyIn("You are safe for now traveler! ", {limit: ['w']});
-    }
-}
-
-// Function that chooses one of the 3 enemies to attack the player 
-function enemyAmbush(){
-    let enemySelected = Math.floor(Math.random() * enemiesArr.length);
-    if(enemySelected == 0){
-        console.log(enemiesArr[0]);
-        fight();
-    } else if(enemySelected == 1){
-        console.log(enemiesArr[1]);
-        fight();
-    } else if(enemySelected == 2){
-        console.log(enemiesArr[2]);
-        fight();
-    }
-}
-
-// Function that allows player to attack or to run
-function fight(){
-    let attackOrRun = readlineSync.keyIn("You have been attacked! Press 'A' to attack or 'R' to run! ", {limit: ['a', 'r']});
-    if(attackOrRun === 'a'){
-        console.log("You have attacked!")
-    } else if(attackOrRun === 'r'){
-        console.log("You attempted to run!")
-    }
-    
+// Random enemy selector function
+function randomEnemy(){
+    enemySelected = enemiesArr[Math.floor(Math.random() * enemiesArr.length)];
 }
 
 let welcome = readlineSync.keyIn("Welcome, " + player.name + "\nPress 'W' to begin you adventure! ", {limit: ['w']});
@@ -70,9 +38,50 @@ let welcome = readlineSync.keyIn("Welcome, " + player.name + "\nPress 'W' to beg
 // While loop that run through the walk function as long as the players hp is above 0 and the enemy array still has at least one enemy in it
 while(enemiesArr.length > 0 && player.hp > 0){
 if(welcome === 'w'){
-    readlineSync.keyIn("Press W to continue ", {limit: ['w']});
+    readlineSync.keyIn("Press W to walk. ", {limit: ['w']});
     walking();        
 }}
+
+
+// Player walking with 1/3 chance of an enemy attacking.
+function walking(){
+    let attackOrWalk = Math.random() * 100;
+    if(attackOrWalk < 33){
+        randomEnemy();
+        console.log(enemySelected.name, 'attacked!');
+        let attackOrRun = readlineSync.keyIn("You have been attacked! Press 'A' to attack or 'R' to run! ", {limit: ['a', 'r']});
+        if(attackOrRun === 'a'){
+            fight();                     
+        } else if(attackOrRun === 'r'){
+            //run(); 
+        } 
+    } else {
+        readlineSync.keyIn("You are safe for now traveler! Press 'W' to walk. ", {limit: ['w']});
+    }
+}
+
+// Function for the fight with the enemy, it should attack the enemy console log its new health, have the enemy attack the player if it's hp is above 0
+// and then repeat as long as the enemy is alive. Once the enemy hp is zero is should continue with the walking function.
+function fight(){
+    let damage = player.attack
+    let enemyDamage = enemySelected.attack
+    let newEnemyHp = enemySelected.hp - damage
+    console.log('You hit the ' + enemySelected.name + ' ' + newEnemyHp);
+    if (newEnemyHp > 0){
+        let newPlayerHp = player.hp - enemyDamage
+        console.log('You have been hit! ' + newPlayerHp);
+    } else if(newEnemyHp <= 0){
+        console.log('You have defeated the ' + enemySelected.name + '!');
+        enemiesArr = enemiesArr.filter(function(enemySelected){
+            console.log(enemySelected)
+        });
+        console.log(enemiesArr)
+        walking();
+    } else if(enemiesArr.length === 0){
+        console.log("Congratulations, " + player.name + ". You have defeated all the enemies and escaped the forest!")
+    }
+}
+
 
 
 
