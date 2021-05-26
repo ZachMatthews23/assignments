f48cad560074b41e722f731f433c7cabe42b8fd0
Zachary Matthews <zacharyyog12@gmail.com>
2021-05-26 09:34:49 -0600

    completed Footer in blog props.

--- Level 3/blog-props/src/Footer.js
+++ Level 3/blog-props/src/Footer.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 function Footer() {
     return(
-        <div>
-            
-        </div>
+        <footer>
+            <div className="footer-links">
+                <ul className="link-list">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div>Copyright © Your Website 2021</div>
+        </footer>
     )
 }
 
--- Level 3/blog-props/src/Navbar.js
+++ Level 3/blog-props/src/Navbar.js
@@ -6,10 +6,10 @@ function Navbar() {
             <a className="nav-link" href="html.com">Start Bootstrap</a>
             <div>
                 <ul className="nav-list">
-                    <li className="list-item"><a href="html.com">HOME</a></li>
-                    <li className="list-item"><a href="html.com">ABOUT</a></li>
-                    <li className="list-item"><a href="html.com">SAMPLE POST</a></li>
-                    <li className="list-item"><a href="html.com">CONTACT</a></li>
+                    <li className="list-item"><a className="navbar-link" href="html.com">HOME</a></li>
+                    <li className="list-item"><a className="navbar-link" href="html.com">ABOUT</a></li>
+                    <li className="list-item"><a className="navbar-link" href="html.com">SAMPLE POST</a></li>
+                    <li className="list-item"><a className="navbar-link" href="html.com">CONTACT</a></li>
                 </ul>
             </div>
         </div>
