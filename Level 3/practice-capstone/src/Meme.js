8ee756248515c4c7f89aabe8fcaac449c282b4df
Zachary Matthews <zacharyyog12@gmail.com>
2021-06-11 15:30:19 -0600

    Created seperate components for form and user generated memes

--- Level 3/practice-capstone/src/Meme.js
+++ Level 3/practice-capstone/src/Meme.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import axios from 'axios'
+import UserMeme from './UserMeme'
+import MemeForm from './MemeForm'
 
 class Meme extends React.Component {
     constructor() {
         super()
         this.state = {
             memeArray: [],
-            randomImg: ""
+            randomImg: "",
+            topText: "",
+            bottomText: "",
+            id: "",
+            userMemes: []
         }
         this.clickHandler = this.clickHandler.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount(){
@@ -18,6 +26,10 @@ class Meme extends React.Component {
                 this.setState({
                     memeArray: memes       
                 })
+                const randNum = Math.floor(Math.random() * 100)
+                console.log(randNum)
+                const loadMeme = this.state.memeArray[randNum].url
+                this.setState({randomImg: loadMeme})
             })
             .catch(err => console.log(err))
     }
@@ -28,13 +40,41 @@ class Meme extends React.Component {
         const loadMeme = this.state.memeArray[randNum].url
         this.setState({randomImg: loadMeme})
     }
+    
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({[name]: value})
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        const newObject = {
+            randomImg: this.state.randomImg,
+            topText: this.state.topText,
+            bottomText: this.state.bottomText,
+            id: this.state.id
+        }
+        this.setState(prevState => ({
+            topText: "",
+            bottomText: "",
+            userMemes: [newObject, ...prevState.userMemes]
+        }))
+    }
 
     render() {
+        const createdMemes = this.state.userMemes.map(meme => <UserMeme userMemes={meme}/>)
 
         return(
             <div>
-                <img style={{width: "400px"}}  src={this.state.randomImg} alt=""/>
-                <button onClick={this.clickHandler}>Generate Meme</button>
+                <MemeForm 
+                    {...this.state}
+                    handleSubmit={this.handleSubmit}
+                    handleChange={this.handleChange}
+                    clickHandler={this.clickHandler}
+                />
+                <div>
+                    {createdMemes}
+                </div>
             </div>
         )
     }
