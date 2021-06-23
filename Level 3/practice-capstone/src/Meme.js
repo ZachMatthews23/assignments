be4b5974345a2425df2b40ab35af95fc1dbd60e7
Zachary Matthews <zacharyyog12@gmail.com>
2021-06-22 20:37:24 -0600

    Added practice capstone updates

--- Level 3/practice-capstone/src/Meme.js
+++ Level 3/practice-capstone/src/Meme.js
@@ -3,20 +3,33 @@ import axios from 'axios'
 import UserMeme from './UserMeme'
 import MemeForm from './MemeForm'
 
+const objectToQueryParam = (obj) => {
+    const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`)
+    return '?' + params.join('&')
+}
+
 class Meme extends React.Component {
     constructor() {
         super()
+        
         this.state = {
             memeArray: [],
             randomImg: "",
             topText: "",
             bottomText: "",
+            topCreatedText: "",
+            bottomCreatedText: "",
             id: "",
+            key: "",
             userMemes: []
+
         }
         this.clickHandler = this.clickHandler.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleDelete = this.handleDelete.bind(this)
+        this.takeToEdit = this.takeToEdit.bind(this)
+        this.submitEdits = this.submitEdits.bind(this)
     }
 
     componentDidMount(){
@@ -24,21 +37,25 @@ class Meme extends React.Component {
             .then(res => {
                 const {memes} = res.data.data
                 this.setState({
-                    memeArray: memes       
+                    memeArray: memes 
                 })
                 const randNum = Math.floor(Math.random() * 100)
-                console.log(randNum)
-                const loadMeme = this.state.memeArray[randNum].url
-                this.setState({randomImg: loadMeme})
+                const loadMeme = this.state.memeArray[randNum]
+                this.setState({
+                    randomImg: loadMeme.url,
+                    id: loadMeme.id,
+                    key: Math.random()
+                })
+                console.log(loadMeme.id)
             })
             .catch(err => console.log(err))
     }
 
     clickHandler() {
         const randNum = Math.floor(Math.random() * 100)
-        console.log(randNum)
-        const loadMeme = this.state.memeArray[randNum].url
-        this.setState({randomImg: loadMeme})
+        const loadMeme = this.state.memeArray[randNum]
+        this.setState({randomImg: loadMeme.url, id: loadMeme.id})
+        console.log(loadMeme.id)
     }
     
     handleChange(event) {
@@ -48,33 +65,120 @@ class Meme extends React.Component {
 
     handleSubmit(event) {
         event.preventDefault()
-        const newObject = {
-            randomImg: this.state.randomImg,
-            topText: this.state.topText,
-            bottomText: this.state.bottomText,
-            id: this.state.id
+
+        const params = {
+            template_id: this.state.id,
+            text0: this.state.topText,
+            text1: this.state.bottomText,
+            username: 'ZKktc1234',
+            password: 'KTCktc1234'
         }
-        this.setState(prevState => ({
-            topText: "",
-            bottomText: "",
-            userMemes: [newObject, ...prevState.userMemes]
-        }))
+
+        axios.get(`https://api.imgflip.com/caption_image${objectToQueryParam(params)}`)
+            .then(res => {
+                console.log(res)
+                const newObject = {
+                    randomImg: res.data.data.url,
+                    topText: this.state.topText,
+                    bottomText: this.state.bottomText,
+                    id: this.state.id,
+                    key: Math.random(),
+                    isEditing: false
+                }
+
+                this.setState(prevState => ({
+                    topText: "",
+                    bottomText: "",
+                    userMemes: [newObject, ...prevState.userMemes]
+                })   
+            )})
+            .catch(err => console.log(err))
+    }
+    
+    handleDelete(item) {
+        this.setState({userMemes: this.state.userMemes.filter(el => el.key !== item.key ) })
     }
 
-    render() {
-        const createdMemes = this.state.userMemes.map(meme => <UserMeme userMemes={meme}/>)
 
+    takeToEdit(id){
+        console.log("This is the edit ID " + id)
+        let selectedMeme = this.state.userMemes.find(meme => meme.id === id)
+        selectedMeme.isEditing = true
+
+        let newUserMemes = this.state.userMemes.map(meme =>{
+            if(meme.id === selectedMeme.id){
+                return selectedMeme
+            }
+            else{
+                return meme
+            }
+        })
+
+        this.setState(
+            {userMemes: newUserMemes}
+        )
+    }
+
+    submitEdits(e, id) {
+        e.preventDefault()
+        console.log("this is the slected meme ID " + id)
+        let selectedMeme = this.state.userMemes.find(meme => meme.id === id)
+        selectedMeme.topText = this.state.topCreatedText
+        selectedMeme.bottomText = this.state.bottomCreatedText
+        selectedMeme.isEditing = false
+
+        const editParams = {
+            template_id: selectedMeme.id,
+            text0: this.state.topCreatedText,
+            text1: this.state.bottomCreatedText,
+            username: 'ZKktc1234',
+            password: 'KTCktc1234'
+        }
+
+        axios.get(`https://api.imgflip.com/caption_image${objectToQueryParam(editParams)}`)
+            .then(res => {
+                console.log(res)
+                const editObject = {
+                    randomImg: res.data.data.url,
+                    topText: this.state.topCreatedText,
+                    bottomText: this.state.bottomCreatedText,
+                    id: this.state.id,
+                    key: Math.random(),
+                    isEditing: false
+                }
+
+                this.setState(prevState => ({
+                    topText: "",
+                    bottomText: "",
+                    userMemes: [editObject, ...prevState.userMemes]
+                })    
+            )})
+            .catch(err => console.log(err))
+    }
+    
+
+    render() {
+            const createdMemes = this.state.userMemes.map(meme => 
+                <UserMeme 
+                    userMemes={meme} 
+                    key={meme.key} 
+                    topCreatedText={this.state.topCreatedText} 
+                    bottomCreatedText={this.state.bottomCreatedText}
+                    handleDelete= {this.handleDelete} 
+                    takeToEdit={this.takeToEdit} 
+                    submitEdits={this.submitEdits} 
+                    handleChange={this.handleChange} 
+                />)
         return(
-            <div>
+            <div className="form">
                 <MemeForm 
                     {...this.state}
                     handleSubmit={this.handleSubmit}
                     handleChange={this.handleChange}
                     clickHandler={this.clickHandler}
                 />
-                <div>
+                <br />
                     {createdMemes}
-                </div>
             </div>
         )
     }
--- Level 3/practice-capstone/src/UserMeme.js
+++ Level 3/practice-capstone/src/UserMeme.js
@@ -1,13 +1,48 @@
 import React from 'react'
 
 function UserMeme(props) {
+    console.log("Meme Id " + props.userMemes.id)
+    
     return(
-        <div className="meme">
-            <img src={props.userMemes.randomImg} alt=""/>
-            <h2 className="top">{props.userMemes.topText}</h2>
-            <h2 className="bottom">{props.userMemes.bottomText}</h2>
+        props.userMemes.isEditing === false?
+        <div className="createdMeme">
+            <div>
+                <img className="image" src={props.userMemes.randomImg} alt=""/>
+            </div>
+            <div className="buttons">
+                <button className="editBtn" onClick={() => props.takeToEdit(props.userMemes.id)}>Edit Text</button>
+                <button className="deleteBtn" onClick={() => props.handleDelete(props.userMemes)}>Delete</button>
+            </div>
+            {/* <h2 className="topCreated">{props.userMemes.topText}</h2>
+            <h2 className="bottomCreated">{props.userMemes.bottomText}</h2> */}
+            
         </div>
+        :
+    
+        <form name="meme-inputs" className="user-meme-form" onSubmit={(e) => props.submitEdits(e, props.userMemes.id)}>
+            <input
+                className="input"
+                type="text"
+                name="topCreatedText"
+                placeholder="Top Text"
+                value={props.topCreatedText}
+                onChange={props.handleChange}    
+            />
+            <input
+                className="input"
+                type="text"
+                name="bottomCreatedText"
+                placeholder="Bottom Text"
+                value={props.bottomCreatedText}
+                onChange={props.handleChange} 
+            />
+            <button className="button">Submit Edits</button>
+        </form>        
+    
+        
+        
     )
+   
 }
 
 export default UserMeme
