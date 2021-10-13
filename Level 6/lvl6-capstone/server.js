d1dfe50f23b29b12885626114af63d5c4a392a38
Zachary Matthews <zacharyyog12@gmail.com>
2021-10-13 11:25:15 -0600

    Initialized back-end

---
+++ Level 6/lvl6-capstone/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const expressJwt = require('express-jwt')
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+mongoose.connect(
+    'mongodb://localhost:27017/lvl6-capstone',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    () => console.log('connected to the DB')
+)
+
+app.use('/auth', require('./routes/authRouter.js'))
+app.use('/api', expressJwt({ secret: process.env.SECRET, algorithms: ['HS256'] }))
+app.use('/api/workout', require('./routes/workoutRouter.js'))
+app.use('/api/forum', require('./routes/postRouter.js'))
+app.use('/api/forum/comment', require('./routes/commentRouter.js'))
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    if(err.name === "UnauthorizedError"){
+        res.status(err.status)
+    }
+    return res.send({errMsg: err.message})
+})
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000')
+})
+
